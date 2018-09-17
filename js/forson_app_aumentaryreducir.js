2b95b02565c6966139edd6931f01bc90b36b4f57
Mauricio Makhoul <mauromakhoul@gmail.com>
2018-09-17 12:25:51 +0200

    añadido un div donde insertar botones

--- js/forson_app_aumentaryreducir.js
+++ js/forson_app_aumentaryreducir.js
@@ -6,11 +6,11 @@ nodo.setAttribute("id", "aumentar");
 nodo.setAttribute("onclick", "setFont()");
 var t = document.createTextNode("Aumentar");
 nodo.appendChild(t);
-var aumentar = document.getElementById("compatriots");
+var aumentar = document.getElementById("todo");
 aumentar.insertBefore(nodo, aumentar.childNodes[0]);
 /*Método que incrementa el tamaño de la fuente*/
 function setFont() {
-   var aumentar = document.getElementById("compatriots");
+   var aumentar = document.getElementById("todo");
    var style = window.getComputedStyle(aumentar, null).getPropertyValue('font-size');
    var fontSize = parseFloat(style);
    fontSize = fontSize+4;
