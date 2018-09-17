f6017593b578b7d317342f8a012eb22c25f97d16
Mauricio Makhoul <mauromakhoul@gmail.com>
2018-09-17 10:20:19 +0200

    La app que maneja el tamaño de las fuentes

---
+++ js/forson_app_aumentaryreducir.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+<script>
+/*Creamos un elemento boton*/
+var nodo = document.createElement("BUTTON")
+nodo.setAttribute("id", "aumentar");
+nodo.setAttribute("onclick", "setFont()");
+var t = document.createTextNode("Aumentar");
+nodo.appendChild(t);
+var aumentar = document.getElementById("compatriots");
+aumentar.insertBefore(nodo, aumentar.childNodes[0]);
+/*Método que incrementa el tamaño de la fuente*/
+function setFont() {
+   var aumentar = document.getElementById("compatriots");
+   var style = window.getComputedStyle(aumentar, null).getPropertyValue('font-size');
+   var fontSize = parseFloat(style);
+   fontSize = fontSize+4;
+   var cadena = fontSize.toString() + "px";
+   aumentar.style.fontSize = cadena; 
+}
+
+</script>
